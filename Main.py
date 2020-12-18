5ca1d45d743fa8583126afe0ebbefea92b057b92
{Zane <{Zanelesley@ou.edu}>
2020-12-18 01:55:55 -0600

    optimized_strategy working

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ from Card import Card
 import random
 
 #Amount of cards/prisoners.
-AMOUNT_TO_FIND = 10
-AMOUNT_OF_TRIES = 5
+AMOUNT_TO_FIND = 100
+AMOUNT_OF_TRIES = 50
 
 #Creates list of prisoner object
 prisoners = [Prisoner(i, False) for i in range(AMOUNT_TO_FIND)]
@@ -44,27 +44,33 @@ def optimized_strategy():
     #Create list of cards
     cards = [Card(i) for i in range(AMOUNT_TO_FIND)]
     random.shuffle(cards)
-   
+    j = 0
+
     for prisoner in prisoners:
         i = 0
-        if prisoner.number == cards[prisoner.number].number:
-            print(f"number found. prisoner = {prisoner.number} amount of tries = {i}")
-            break
         #TODO Something in here is broke 
-        else:
-            new_number = cards[prisoner.number].number
-            while prisoner.number != new_number or i < AMOUNT_OF_TRIES:
-                print(f"number not found. prisoner = {prisoner.number} card = {new_number} amount of tries = {i}")
-                temp_new_number = cards[new_number].number
-                new_number = temp_new_number
-                i += 1
-            break
-    return cards
-               
+        new_number = cards[prisoner.number].number
+        #Look into why this works....
+        while not (prisoner.number == new_number or i > AMOUNT_OF_TRIES):
+            #print(f"number not found. prisoner = {prisoner.number} card = {new_number} amount of tries = {i}")
+            temp_new_number = cards[new_number].number
+            new_number = temp_new_number
+            i += 1
+            #break
+        if prisoner.number == new_number:
+            #print(f"number found. prisoner = {prisoner.number} amount of tries = {i}")
+            j += 1
+
+    return j
+
+success = 0
+fail = 0               
 for x in range(1000):
     result = optimized_strategy()
-    
-    if len(result) == 0:
-        print("success")
+    if result == AMOUNT_TO_FIND:
+        success += 1
     else:
-        print("fail")     
+        fail += 1
+
+print(f"success : {success}")
+print(f"fail: {fail}")
